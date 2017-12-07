d8e47cf8fe55405c608021686029aa3b7e671ef4
mnisdh <mnisdh@nate.com>
2017-12-07 15:47:17 +0900

    구글맵 자세히보기 기능 (#30)

--- app/src/main/java/a/talenting/com/talenting/common/Constants.java
+++ app/src/main/java/a/talenting/com/talenting/common/Constants.java
@@ -23,4 +23,8 @@ public class Constants {
     public static final int GALLERY_REQ = 801;
     public static final int LOGIN_REQ = 900;
     public static final int PERMISSION_REQ = 901;
+
+    public static final String EXT_LAT = "lat";
+    public static final String EXT_LNG = "lng";
+    public static final String EXT_IS_DETAIL = "isdetail";
 }
