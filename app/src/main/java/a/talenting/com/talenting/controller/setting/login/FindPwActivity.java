1417e610b99e9859409dbba3c11a0028afcaeb24
DESKTOP-DR5OTRL\user <kps990515@gmail.com>
2017-12-13 11:04:34 +0900

    Spinner County, Language, 비번찾기 (#59)

--- app/src/main/java/a/talenting/com/talenting/controller/setting/login/FindPwActivity.java
+++ app/src/main/java/a/talenting/com/talenting/controller/setting/login/FindPwActivity.java
@@ -11,11 +11,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import a.talenting.com.talenting.R;
+import a.talenting.com.talenting.domain.DomainManager;
+import a.talenting.com.talenting.domain.user.PwMissing;
+import a.talenting.com.talenting.domain.user.PwMissingResponse;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class FindPwActivity extends AppCompatActivity {
 
@@ -23,6 +30,8 @@ public class FindPwActivity extends AppCompatActivity {
     private EditText edit_findEmail;
     private TextView txt_emailalert;
     private Button btn_sendEmail;
+    private EditText edit_fname;
+    private EditText edit_lname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,32 @@ public class FindPwActivity extends AppCompatActivity {
         edit_findEmail = findViewById(R.id.edit_findEmail);
         txt_emailalert = findViewById(R.id.txt_emailalert);
         btn_sendEmail = findViewById(R.id.btn_sendEmail);
+        edit_fname = findViewById(R.id.edit_fname);
+        edit_lname = findViewById(R.id.edit_lname);
+    }
+
+    public void send(View view){
+        PwMissing pw_missing = new PwMissing();
+        pw_missing.setEmail(edit_findEmail.getText().toString());
+        pw_missing.setFirst_name(edit_fname.getText().toString());
+        pw_missing.setLast_name(edit_lname.getText().toString());
+
+        Observable<PwMissingResponse> observable = DomainManager.getUserApiService().pwMissing(pw_missing);
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> {
+                    if(result.isSuccess()){
+                        Toast.makeText(this, "Send Email to "+edit_findEmail.getText().toString()+"!", Toast.LENGTH_SHORT).show();
+                        success();
+                    }
+                    else Toast.makeText(this, result.getMsg(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private void success(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
---
+++ app/src/main/java/a/talenting/com/talenting/domain/user/PwMissing.java
@@ -0,0 +1,42 @@
+package a.talenting.com.talenting.domain.user;
+
+/**
+ * Created by user on 2017-12-12.
+ */
+
+public class PwMissing {
+    private String first_name;
+
+    private String email;
+
+    private String last_name;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [first_name = " + first_name + ", email = " + email + ", last_name = " + last_name + "]";
+    }
+}
